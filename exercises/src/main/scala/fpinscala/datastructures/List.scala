38c9143df81389b4e170f52296045b09a94b21a0
tthatcher <thatcher.travis@gmail.com>
2016-01-10 23:12:53 -0600

    Chapter 3 -  Functional Data Structures
    Completed exercises

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -9,60 +9,368 @@ case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List { // `List` companion object. Contains functions for creating and working with lists.
   def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
-    case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+    case Nil         => 0 // The sum of the empty list is 0.
+    case Cons(x, xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
   }
 
   def product(ds: List[Double]): Double = ds match {
-    case Nil => 1.0
+    case Nil          => 1.0
     case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
+    case Cons(x, xs)  => x * product(xs)
   }
 
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
-  val x = List(1,2,3,4,5) match {
-    case Cons(x, Cons(2, Cons(4, _))) => x
-    case Nil => 42
+  val x = List(1, 2, 3, 4, 5) match {
+    case Cons(x, Cons(2, Cons(4, _)))          => x
+    case Nil                                   => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
-    case Cons(h, t) => h + sum(t)
-    case _ => 101
+    case Cons(h, t)                            => h + sum(t)
+    case _                                     => 101
   }
 
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
-      case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
+      case Nil        => a2
+      case Cons(h, t) => Cons(h, append(t, a2))
     }
 
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
     as match {
-      case Nil => z
+      case Nil         => z
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
 
   def sum2(ns: List[Int]) =
-    foldRight(ns, 0)((x,y) => x + y)
+    foldRight(ns, 0)((x, y) => x + y)
 
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  //Exercise 2
+  def tail[A](l: List[A]): List[A] = l match {
+    case Cons(x, xs) => xs
+    case Nil         => throw new IllegalArgumentException("Can't get tail of nil list")
+  }
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def head[A](l: List[A]): A = l match {
+    case Cons(x, xs) => x
+    case Nil         => throw new IllegalArgumentException("Can't get head of nil list")
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  //Exercise 3
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Cons(x, xs) => Cons(h, xs)
+    case Nil         => throw new IllegalArgumentException("Can't set head of nil list")
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  //Some other attempt I did
+  def dropTailRecursiveDecomp[A](list: List[A], n: Int): List[A] = {
+    if (list == Nil) {
+      throw new IllegalArgumentException("Can't drop elements from nil list")
+    }
+    @annotation.tailrec
+    def go(l: List[A], i: Int): List[A] = {
+      if (i == n) {
+        l
+      } else {
+        go(tail(l), i + 1)
+      }
+    }
+    go(list, 0)
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  //Exercise 4
+  def drop[A](l: List[A], n: Int): List[A] = {
+    def go(l: List[A], i: Int): List[A] = l match {
+      case Cons(x, xs) => if (n != i) go(xs, i + 1) else l
+      case Nil         => throw new IllegalArgumentException("Can't drop elements from nil list")
+    }
+    go(l, 0)
+  }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  //Some other attempt I did for exercise 5
+  def dropWhileTailRecursive[A](l: List[A], f: A => Boolean): List[A] = {
+    if (l == Nil) {
+      throw new IllegalArgumentException("Can't drop elements from nil list")
+    }
+    @annotation.tailrec
+    def go(l: List[A]): List[A] = {
+      if (l == Nil) {
+        Nil
+      } else if (f(List.head(l))) {
+        go(List.tail(l))
+      } else {
+        l
+      }
+    }
+    go(l)
+  }
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  //Exercise 5
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(x, xs) => if (f(x)) dropWhile(xs, f) else l
+    case Nil         => l
+  }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  //Exercise 6
+  def init[A](l: List[A]): List[A] = l match {
+    case Cons(x, Nil) => Nil
+    case Cons(x, xs)  => Cons(x, init(xs))
+    case _            => Nil
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  //Exercise 9
+  def lengthFoldRight[A](as: List[A]): Int = {
+    foldRight[A, Int](as, 0)((x, y) => y + 1)
+  }
+
+  //Exercise 10. Note tailrec annotation.
+  @annotation.tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = as match {
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    case Nil         => z
+  }
+
+  //Exercise 11
+  def sumfoldLeft(as: List[Int]): Int = {
+    foldLeft[Int, Int](as, 0)(_ + _)
+  }
+
+  //Exercise 11
+  def product(as: List[Int]): Int = {
+    foldLeft[Int, Int](as, 1)(_ * _)
+  }
+
+  //Exercise 11
+  def length[A](as: List[A]): Int = {
+    foldLeft[A, Int](as, 0)((x, y) => x + 1)
+  }
+
+  //Exercise 12
+  def reverse[A](as: List[A]): List[A] = {
+    foldLeft(as, Nil: List[A])((x, y) => Cons(y, x))
+  }
+
+  def appendFoldRight[A](as: List[A], r: List[A]): List[A] = {
+    foldRight(as, r)((x, y) => Cons(x, y))
+  }
+
+  //The one from the answer key is better.
+  //Exercise 15
+  def flattenMine[A](as: List[List[A]]): List[A] = {
+
+    def flattenHelper[A](ls: List[A], as: List[List[A]]): List[A] = {
+      ls match {
+        case Cons(x, xs) => Cons(x, Cons(List.head(xs), flattenHelper(List.tail(xs), as)))
+        case Nil         => flattenMine(as)
+      }
+    }
+    as match {
+      case Cons(x, xs) => Cons(List.head(x), flattenHelper(List.tail(x), xs))
+      case Nil         => Nil
+    }
+  }
+
+  //From answers. 
+  def concat[A](l: List[List[A]]): List[A] = foldRight(l, Nil: List[A])(append)
+
+  //Exercise 16
+  def addOne(l: List[Int]): List[Int] = l match {
+    case Cons(x, xs) => Cons(x + 1, addOne(xs))
+    case Nil         => Nil
+  }
+
+  //Exercise 17
+  def doubleToString(l: List[Double]): List[String] = l match {
+    case Nil         => Nil
+    case Cons(x, xs) => Cons(x.toString, doubleToString(xs))
+  }
+
+  //Exercise 18
+  def map[A, B](l: List[A])(f: A => B): List[B] = l match {
+    case Nil         => Nil
+    case Cons(x, xs) => Cons(f(x), map(xs)(f))
+  }
+
+  //Exercise 19
+  def filter[A](l: List[A])(f: A => Boolean): List[A] = l match {
+    case Nil         => Nil
+    case Cons(x, xs) => if (f(x)) Cons(x, filter(xs)(f)) else filter(xs)(f)
+  }
+
+  //Exercise 20
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = concat(map(as)(f))
+
+  //Exercise 21
+  def filterFlatMap[A](l: List[A])(f: A => Boolean): List[A] = flatMap(l)(x => if (f(x)) List(x) else Nil)
+
+  //Exercise 22
+  def addLists(l: List[Int], r: List[Int]): List[Int] = (l, r) match {
+    case (Cons(x, xs), Cons(y, ys)) => Cons(x + y, addLists(xs, ys))
+    case (Nil, _)                   => Nil
+    case (_, Nil)                   => Nil
+  }
+
+  //Exercise 23
+  def zipWith[A](l: List[A], r: List[A])(f: (A, A) => A): List[A] = (l, r) match {
+    case (Cons(x, xs), Cons(y, ys)) => Cons(f(x, y), zipWith(xs, ys)(f))
+    case (Nil, _)                   => Nil
+    case (_, Nil)                   => Nil
+  }
+
+  //Exercise 24
+  def hasSubsequence[A](l: List[A], r: List[A]): Boolean = (l, r) match {
+    case (Nil, _)                   => false
+    case (Cons(x, xs), Cons(y, ys)) => if (x == y) true else hasSubsequence(l, ys)
+    case (Cons(x, xs), Nil)         => hasSubsequence(xs, r)
+  }
+}
+
+object ListExercises extends App {
+
+  //Exercise 1
+  //3
+
+  //I'm grouping each exercise into a closure
+
+  //I implemented many of my list functions in such a way where the nil case throws an exception. Probably not ideal.
+  val exercise2 = {
+    val list: List[Int] = List(1, 2, 3)
+    println(s"Tail of list ${List.tail(list)}")
+    try {
+      List.tail(Nil)
+    } catch {
+      case ex: Exception => println(s"Nil list throws exception ${ex}")
+    }
+  }
+
+  val exercise3 = {
+    val list: List[Int] = List(1, 2, 3)
+    println(s"Set head to 100 ${List.setHead(list, 100)}")
+    try {
+      List.setHead(Nil, 100)
+    } catch {
+      case ex: Exception => println(s"Nil list throws exception ${ex}")
+    }
+  }
+  val exercise4 = {
+    val list: List[Int] = List(1, 2, 3, 4, 5)
+    println("Drop 3 elements ${List.drop(list, 3)}")
+    try {
+      List.drop(Nil, 100)
+    } catch {
+      case ex: Exception => println(s"Nil list throws exception ${ex}")
+    }
+  }
+
+  val exercise5 = {
+    val list: List[Int] = List(1, 2, 3, 4, 5)
+    def not3(x: Int) = x != 3
+    println(s"Drop while not 3 ${List.dropWhile(list, not3)}")
+    println(s"Drop while not 10 ${List.dropWhile(list, (x: Int) => x != 10)}")
+    println(s"Drop with nil input ${List.dropWhile(Nil, not3)}")
+  }
+
+  val exercise6 = {
+    val list: List[Int] = List(1, 2, 3, 4, 5)
+    println(s"All but last of list ${List.init(list)}")
+    println(s"All but last of nil list ${List.init(Nil)}")
+  }
+
+  //Exercise 7
+  //Answer: No. Fold right always evaluates the entire list. You can't short circuit it as is.
+
+  val exercise8 = {
+    println(s"Same list ${List.foldRight(List(1, 2, 3), Nil: List[Int])(Cons(_, _))}")
+  }
+
+  val exercise9 = {
+    val list = List(1, 2, 3, 4, 5, 6)
+    println(s"Length: ${List.lengthFoldRight(list)}")
+  }
+
+  val exercise10 = {
+    //Fold right is not tail recursive and will explode. I'm not going to do this in code.
+    val list: List[Int] = List(1, 2, 3, 4, 5)
+    println(s"Fold left add ${List.foldLeft(list, 2)(_ + _)}")
+  }
+
+  val exercise11 = {
+    val list: List[Int] = List(1, 2, 3, 4, 5)
+    println(s"Sum fold left: ${List.sum(list)}")
+    println(s"Product fold left: ${List.product(list)}")
+    println(s"Length fold left: ${List.length(list)}")
+  }
+
+  val exercise12 = {
+    val list: List[Int] = List(1, 2, 3, 4, 5)
+    println(s"Not reversed: ${list}")
+    println(s"Reversed: ${List.reverse(list)}")
+  }
+
+  //Exercise 13
+  //Answer: Reverse the list
+
+  val exercise14 = {
+    val list: List[Int] = List(1, 2, 3, 4, 5)
+    println(s"Append using fold right: ${List.appendFoldRight(list, List(2))}")
+  }
+
+  val exercise15 = {
+    val list = List(List(1, 2, 3), List(4, 5, 6))
+    println(s"Flatten: ${List.flattenMine(list)}")
+  }
+
+  val exercise16 = {
+    val list = List(1, 2, 3, 4)
+    println(s"Add one: ${List.addOne(list)}")
+  }
+
+  val exercise17 = {
+    val list: List[Double] = List(1, 2, 3, 4)
+    println(s"Dbl to string: ${List.doubleToString(list)}")
+  }
+
+  val exercise18 = {
+    val list = List(1, 2, 3, 4)
+    println(s"Map plus 5: ${List.map(list)(_ + 5)}")
+  }
+
+  val exercise19 = {
+    val list = List(1, 2, 3, 4, 4, 1)
+    println(s"Filtering on 4: ${List.filter(list)(_ == 4)}")
+  }
+
+  val exercise20 = {
+    val list = List(1, 2, 3)
+    println(s"Flatmap map to List(x,x) : ${List.flatMap(list)(i => List(i, i))}")
+  }
+
+  val exercise21 = {
+    val list = List(1, 2, 3, 4, 4, 1)
+    println(s"Filtering using flatmap on 1: ${List.filterFlatMap(list)(_ == 1)}")
+  }
+
+  val exercise22 = {
+    val list1 = List(1, 2, 3, 4, 4, 1)
+    val list2 = List(1, 2, 3, 4, 4, 99)
+    println(s"Adding lists ${List.addLists(list1, list2)}")
+  }
+
+  val exercise23 = {
+    val list1 = List(1, 2, 3, 4, 4, 1)
+    val list2 = List(1, 2, 3, 4, 4, 99)
+    println(s"Subtracting lists using zipwith ${List.zipWith(list1, list2)(_ - _)}")
+  }
+
+  val exercise24 = {
+    val list1 = List(1, 2, 3, 4, 4, 1)
+    val list2 = List(1, 2, 3, 4, 4, 99)
+    val list3 = List(89, 20, 33)
+    println(s"Has subsequence ${List.hasSubsequence(list1, list2)}")
+    println(s"Has subsequence ${List.hasSubsequence(list1, list3)}")
+  }
 }
--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -5,9 +5,90 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
-object Tree {
+  object Tree {
+  
+    //Exercise 25
+    def size[A](t: Tree[A]): Int = t match {
+      case Leaf(_)      => 1
+      case Branch(x, y) => 1 + size(x) + size(y)
+    }
 
+    //Exercise 26
+    def maximum(t: Tree[Int]): Int = t match {
+      case Leaf(x)      => x
+      case Branch(x, y) => maximum(x) max maximum(y)
+    }
 
+    //Exercise 27
+    def depth[A](t: Tree[A]): Int = t match {
+      case Leaf(_)      => 0
+      case Branch(x, y) => (depth(x) max depth(y)) + 1
+    }
 
+    //Exercise 28
+    def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+      case Leaf(x)      => Leaf(f(x))
+      case Branch(x, y) => Branch(map(x)(f), map(y)(f))
+    }
 
+    //Exercise 29
+    def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+      case Leaf(x)      => f(x)
+      case Branch(x, y) => g(fold(x)(f)(g), fold(y)(f)(g))
+    }
+    
+    //Exercise 29
+    def sizeFold[A](t: Tree[A]): Int = fold(t)(x => 1)(_ + _ + 1)
+
+    //Exercise 29
+    def maximumFold(t: Tree[Int]): Int = fold(t)(x => x)(_ max _)
+
+    //Exercise 29
+    def depthFold(t: Tree[Int]): Int = fold(t)(x => 0)((x, y) => (x max y) + 1)
+
+    //Exercise 29
+    def mapFold[A, B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(x => Leaf(f(x)): Tree[B])(Branch(_, _))
+
+  }
+
+object TreeExercise extends App {
+    val exercise25 = {
+    val tree = Branch(Branch(Leaf(13), Leaf(3)), Leaf(45))
+    println(s"Number of nodes ${Tree.size(tree)}")
+
+    val tree2 = Branch(Leaf(1), Leaf(45))
+    println(s"Number of nodes ${Tree.size(tree2)}")
+
+    val tree3 = Leaf(1)
+    println(s"Number of nodes ${Tree.size(tree3)}")
+  }
+
+  val exercise26 = {
+    val tree = Branch(Branch(Leaf(100), Leaf(3)), Leaf(45))
+    println(s"Max of tree is  ${Tree.maximum(tree)}")
+  }
+
+  val exercise27 = {
+    val tree1 = Branch(Branch(Leaf(100), Branch(Leaf(5), Leaf(3))), Leaf(45))
+    println(s"Depth of tree is  ${Tree.depth(tree1)}")
+
+    val tree2 = Branch(Leaf(1), Leaf(2))
+    println(s"Depth of tree is  ${Tree.depth(tree2)}")
+  }
+
+  val exercise28 = {
+    val tree = Branch(Branch(Leaf(100), Branch(Leaf(5), Leaf(3))), Leaf(45))
+    println(s"Mapped tree with 10 added is  ${Tree.map(tree)(_ + 10)}")
+  }
+
+  val exercise29 = {
+    val tree1 = Branch(Branch(Leaf(13), Leaf(3)), Leaf(45))
+    println(s"Number of nodes ${Tree.sizeFold(tree1)}")
+    val tree2 = Branch(Branch(Leaf(100), Leaf(71000)), Leaf(33))
+    println(s"Max of tree is  ${Tree.maximumFold(tree2)}")
+    val tree3 = Branch(Branch(Leaf(100), Branch(Branch(Leaf(5), Leaf(3)), Leaf(3))), Leaf(45))
+    println(s"Depth of tree is  ${Tree.depthFold(tree3)}")
+    val tree4 = Branch(Branch(Leaf(100), Branch(Leaf(5), Leaf(3))), Leaf(45))
+    println(s"Mapped tree with 10 multiplied is  ${Tree.mapFold(tree4)(_ * 10)}")
+  }
 }
