b2a45c167359306e0d8937e1e8da99e95ac3dc30
tthatcher <thatcher.travis@gmail.com>
2016-01-11 21:27:23 -0600

    Chapter 4 - Error Handling
    Completed exercises

--- exercises/src/main/scala/fpinscala/errorhandling/Either.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Either.scala
@@ -1,37 +1,87 @@
 package fpinscala.errorhandling
 
+import scala.{ Option => _, Either => _, Left => _, Right => _, _ } // hide std library `Option` and `Either`, since we are writing our own in this chapter
 
-import scala.{Option => _, Either => _, Left => _, Right => _, _} // hide std library `Option` and `Either`, since we are writing our own in this chapter
+case class Left[+E](get: E) extends Either[E, Nothing]
+case class Right[+A](get: A) extends Either[Nothing, A]
 
-sealed trait Either[+E,+A] {
- def map[B](f: A => B): Either[E, B] = sys.error("todo")
+sealed trait Either[+E, +A] {
+  //Exercise 6
+  def map[B](f: A => B): Either[E, B] = this match {
+    case Right(x)   => Right(f(x))
+    case x: Left[E] => x
+  }
 
- def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = sys.error("todo")
+  //Exercise 6
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+    case x: Right[B] => x
+    case _           => b
+  }
 
- def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = sys.error("todo")
+  //Exercise 6
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+    case Right(x)   => f(x)
+    case x: Left[E] => x
+  }
 
- def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = sys.error("todo")
+  //Exercise 6
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = flatMap(x => b.map(y => f(x, y)))
 }
-case class Left[+E](get: E) extends Either[E,Nothing]
-case class Right[+A](get: A) extends Either[Nothing,A]
 
 object Either {
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = sys.error("todo")
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] = traverse(es)(x => x)
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = sys.error("todo")
+  //Issue with currying I think, if this wasn't curried it would be ok and I could overload
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = as match {
+    case x :: xs => f(x).flatMap(y => traverse(xs)(f) map (z => y :: z))
+    case Nil     => Right(Nil)
+  }
 
-  def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
-    if (xs.isEmpty) 
+  def mean(xs: IndexedSeq[Double]): Either[String, Double] =
+    if (xs.isEmpty)
       Left("mean of empty list!")
-    else 
+    else
       Right(xs.sum / xs.length)
 
-  def safeDiv(x: Int, y: Int): Either[Exception, Int] = 
+  def safeDiv(x: Int, y: Int): Either[Exception, Int] =
     try Right(x / y)
     catch { case e: Exception => Left(e) }
 
   def Try[A](a: => A): Either[Exception, A] =
     try Right(a)
     catch { case e: Exception => Left(e) }
+
+}
+
+object EitherExercises extends App {
+import fpinscala.errorhandling.Either._
+  val exercise6 = {
+    val l: Either[String, Int] = Left("Bad value")
+    val r: Either[String, Int] = Right(5)
+    println(s"Map right ${r.map(_ + 5)}")
+    println(s"Map left ${l.map(_ + 5)}")
+    println(s"Or else 10 right ${r.orElse(Right(10))}")
+    println(s"Or else 10 left ${l.orElse(Right(10))}")
+    println(s"Flatmap right ${r.flatMap(x => Right(x + 5))}")
+    println(s"Flatmap left ${l.flatMap(x => Right(x + 5))}")
+    println(s"Map2 right ${r.map2(Right(10))(_ + _)}")
+    println(s"Map2 right with left input ${r.map2(Left("Bad value again"): Either[String, Int])(_ + _)}")
+    println(s"Map2 left ${l.map2(Right(10))(_ + _)}")
+  }
+
+  val exercise7 = {
+    val optList1 = List(Right(1), Right(50), Right(30))
+    val optList2 = List(Right(1), Left("This is bad input"), Right(30))
+    println(s"Sequence with good input ${sequence(optList1)}")
+    println(s"Sequence with bad input ${sequence(optList2)}")
+    val list1 = List(1, 2, 3, 5, -10)
+    val list2 = List(1, 2, 3, 5, 10)
+    def gt0(x: Int) = if (x > 0) Right(x) else Left("Bad input")
+    println(s"Traverse with bad input ${traverse(list1)(gt0)}")
+    println(s"Traverse with good input ${traverse(list2)(gt0)}")
+  }
+
+  //For 4.8 We'd need to accumulate Errors in a list instead of just taking one, and modify the 
+  // functions that operate on either to add errors to that sequence
 
 }
--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,19 +1,30 @@
 package fpinscala.errorhandling
 
-
-import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
+import scala.{ Option => _, Some => _, Either => _, _ } // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  //Exercise 1
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(x) => Some(f(x))
+    case None    => None
+  }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  //Exercise 1
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f) getOrElse None
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  //Exercise 1
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(x) => x
+    case None    => default
+  }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  //Exercise 1
+  def orElse[B >: A](default: => B): Option[B] = Some(getOrElse(default))
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  //Exercise 1
+  def filter(f: A => Boolean): Option[A] = flatMap(x => if (f(x)) this else None)
 }
+
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
@@ -23,26 +34,106 @@ object Option {
     try {
       val x = 42 + 5
       x + y
-    }
-    catch { case e: Exception => 43 } // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
+    } catch { case e: Exception => 43 } // A `catch` block is just a pattern matching block like the ones we've seen. `case e: Exception` is a pattern that matches any `Exception`, and it binds this value to the identifier `e`. The match returns the value 43.
   }
 
   def failingFn2(i: Int): Int = {
     try {
       val x = 42 + 5
       x + ((throw new Exception("fail!")): Int) // A thrown Exception can be given any type; here we're annotating it with the type `Int`
-    }
-    catch { case e: Exception => 43 }
+    } catch { case e: Exception => 43 }
   }
 
-  def mean(xs: Seq[Double]): Option[Double] =
-    if (xs.isEmpty) None
-    else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
+  def variance(xs: Seq[Double]): Option[Double] = {
+    def mean(xs: Seq[Double]): Option[Double] = Some(xs.sum / xs.length)
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  //Exercise 3
+  //Initially I did this with pattern matching but I've grown more comfortable with the idea of chaining flatmaps
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a.flatMap(x => b.map(y => f(x, y)))
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+  //Exercise 4
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = {
+    def go(a: List[Option[A]]): List[A] = a match {
+      case Some(x) :: Nil => List(x)
+      case Some(x) :: xs  => Some(x).map(y => x :: go(xs)).getOrElse(Nil)
+      case _              => Nil
+    }
+    val newList = go(a)
+    if (newList.size == a.size) Some(newList) else None
+  }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+  //Exercise 4, another way to do it
+  def sequence_1[A](a: List[Option[A]]): Option[List[A]] =
+    a match {
+      case Nil     => Some(Nil)
+      case x :: xs => x flatMap (y => sequence_1(xs) map (z => y :: z))
+    }
+
+  //Exercise 5
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil     => Some(Nil)
+    case x :: xs => f(x) flatMap (y => traverse(xs)(f) map (z => y :: z))
+  }
+  
+  //Exercise 5, sequence in terms of traverse
+  def sequenceInTermsOfTraverse[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(x => x)
+  
+  def tryThis[A](a: => A): Option[A] = try Some(a) catch { case e: Exception => None }
+}
+
+object OptionExercises extends App {
+  import fpinscala.errorhandling.Option._
+  val exercise1 = {
+    val opt1 = Some(1)
+    val opt2: Option[Int] = None
+    println(s"Map 1 to some ${opt1.map(_ + 1)}")
+    println(s"Map 1 to none ${opt2.map(_ + 1)}")
+    println(s"Flatmap Some(1) ${opt1.flatMap(x => Some(x + 1))}")
+    println(s"Flatmap None ${opt2.flatMap(x => Some(x + 1))}")
+    println(s"Get or else 2 Some(1) ${opt1.getOrElse(2)}")
+    println(s"Get or else 2 None ${opt2.getOrElse(2)}")
+    println(s"Or else Some(2) Some(1) ${opt1.orElse(2)}")
+    println(s"Or else Some(2) None ${opt2.orElse(2)}")
+    println(s"Filter when ne 1 Some(1) ${opt1.filter(_ != 1)}")
+    println(s"Filter when eq 1 Some(1) ${opt1.filter(_ == 1)}")
+    println(s"Filter when ne 1 None ${opt2.filter(_ != 1)}")
+  }
+
+  val exercise2 = {
+    //Exercise 2
+    val seq1: Seq[Double] = Seq(1, 2, 3, 4, 5)
+    println(s"Variance of ${variance(seq1)}")
+  }
+
+  val exercise3 = {
+    val opt1 = Some(2)
+    val opt2 = Some(5)
+    val opt3 = map2(opt1, opt2)((x, y) => x * y)
+    println(s"map2 of opt3 is $opt3")
+  }
+
+  val exercise4 = {
+    val list1 = List(Some(1), Some(2), Some(3))
+    println(s"Sequence is ${sequence(list1)}")
+
+    val list2 = List(Some(1), None, Some(3), None)
+    println(s"Sequence is ${sequence(list2)}")
+  }
+
+  val exercise5 = {
+    val list1 = List("1", "2", "3")
+    println(s"Traversal is ${traverse(list1)(x => tryThis { x.toInt })}")
+
+    val list2 = List("1", "2", "Snikes")
+    println(s"Traversal is ${traverse(list2)(x => tryThis { x.toInt })}")
+
+    val list3 = List(Some(1), Some(2), Some(3))
+    println(s"Sequence2 is ${sequenceInTermsOfTraverse(list3)}")
+
+    val list4 = List(Some(1), None, Some(3), None)
+    println(s"Sequence2 is ${sequenceInTermsOfTraverse(list4)}")
+  }
+
 }
